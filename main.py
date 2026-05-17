36b268ba9e82a0efa210f147b09872dd8ee5f859
EmmaAlsh <julialtobello@gmail.com>
2026-05-17 20:26:14 -0300

    implement fruit class and drawing

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-import pygame, sys
+import pygame, sys, random
+from pygame.math import Vector2 
+
+class FRUIT:
+  def __init__(self):
+    self.x = random.randint(0,cell_number - 1)
+    self.y = random.randint(0,cell_number - 1)
+    self.pos = Vector2(self.x,self.y)
+  
+  def draw_fruit(self):
+    fruit_rect = pygame.Rect(int(self.pos.x * cell_size) , int(self.pos.y * cell_size),cell_size,cell_size )
+    pygame.draw.rect(screen, (126,166,114), fruit_rect)
 
 pygame.init()
-screen = pygame.display.set_mode((400, 500))
+cell_size = 40 
+cell_number = 20 
+screen = pygame.display.set_mode((cell_number * cell_size,cell_number * cell_size))
 clock = pygame.time.Clock()
+
+fruit = FRUIT()
 
 while True: 
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       pygame.quit() # apaga pygame 
       sys.exit() # cierra el programa
+
+  screen.fill((214,212,94))
+  fruit.draw_fruit()
   pygame.display.update()
   clock.tick(60) # para que no ande tan rapido
