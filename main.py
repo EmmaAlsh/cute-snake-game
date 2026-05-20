261c608a0e5f4e4ff79db8f69bcb7846ada7670c
EmmaAlsh <julialtobello@gmail.com>
2026-05-19 22:54:37 -0300

    implement snake movement controls

--- main.py
+++ main.py
@@ -47,6 +47,16 @@ while True:
       sys.exit() # cierra el programa
     if event.type == SCREEN_UPDATE:
       snake.move_snake()
+    if event.type == pygame.KEYDOWN: 
+      if event.key == pygame.K_UP:
+        snake.direction = Vector2(0,-1)
+      if event.key == pygame.K_DOWN:
+        snake.direction = Vector2(0,1)
+      if event.key == pygame.K_LEFT:
+        snake.direction = Vector2(-1,0)
+      if event.key == pygame.K_RIGHT:
+        snake.direction = Vector2(1,0)
+
 
   screen.fill((214,212,94))
   fruit.draw_fruit()
