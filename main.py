7918334509fda93b5b4bc08486e49401e34d50d8
EmmaAlsh <julialtobello@gmail.com>
2026-05-25 22:53:05 -0300

    add snake graphics

--- main.py
+++ main.py
@@ -7,6 +7,35 @@ class SNAKE:
     self.direction = Vector2(1,0)
     self.new_block = False
 
+    self.head_up = pygame.image.load('Graphics/head_up.png').convert_alpha()
+    self.head_down = pygame.image.load('Graphics/head_down.png').convert_alpha()
+    self.head_right = pygame.image.load('Graphics/head_right.png').convert_alpha()
+    self.head_left = pygame.image.load('Graphics/head_left.png').convert_alpha()
+
+    self.tail_up = pygame.image.load('Graphics/tail_up.png').convert_alpha()
+    self.tail_down = pygame.image.load('Graphics/tail_down.png').convert_alpha()
+    self.tail_right = pygame.image.load('Graphics/tail_right.png').convert_alpha()
+    self.tail_left = pygame.image.load('Graphics/tail_left.png').convert_alpha()
+
+    self.body_vertical = pygame.image.load('Graphics/body_vertical.png').convert_alpha()
+    self.body_horizontal = pygame.image.load('Graphics/body_horizontal.png').convert_alpha()
+
+    self.body_vertical_bow = pygame.image.load('Graphics/body_vertical_bow.png').convert_alpha()
+    self.body_horizontal_bow = pygame.image.load('Graphics/body_horizontal_bow.png').convert_alpha()
+    self.body_left_bow = pygame.image.load('Graphics/body_left_bow.png').convert_alpha()
+    self.body_right_bow = pygame.image.load('Graphics/body_right_bow.png').convert_alpha()
+
+    self.body_tr_bow = pygame.image.load('Graphics/body_tr_bow.png').convert_alpha()
+    self.body_tl_bow = pygame.image.load('Graphics/body_tl_bow.png').convert_alpha()
+    self.body_br_bow = pygame.image.load('Graphics/body_br_bow.png').convert_alpha()
+    self.body_bl_bow = pygame.image.load('Graphics/body_bl_bow.png').convert_alpha()
+
+    self.body_tr = pygame.image.load('Graphics/body_tr.png').convert_alpha()
+    self.body_tl = pygame.image.load('Graphics/body_tl.png').convert_alpha()
+    self.body_br = pygame.image.load('Graphics/body_br.png').convert_alpha()
+    self.body_bl = pygame.image.load('Graphics/body_bl.png').convert_alpha()
+
+
   def draw_snake(self):
     for block in self.body:
       x_pos = int(block.x * cell_size)
